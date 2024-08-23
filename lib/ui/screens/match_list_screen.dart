274bdc98a04031e8fac637af88297c5bcf531ac0
Eshita75 <163004513+Eshita75@users.noreply.github.com>
2024-08-23 23:23:12 +0600

    success

--- lib/ui/screens/match_list_screen.dart
+++ lib/ui/screens/match_list_screen.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:module20_class2_firebase_app/entities/football.dart';
+
+import '../widgets/FootballScoreCard.dart';
 
 class MatchListScreen extends StatefulWidget {
   const MatchListScreen({super.key});
@@ -8,6 +12,36 @@ class MatchListScreen extends StatefulWidget {
 }
 
 class _MatchListScreenState extends State<MatchListScreen> {
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  List<Football> matchList = [];
+
+
+
+  Future<void> _getFootballMatches ()async {
+    final QuerySnapshot result = await firebaseFirestore.collection('football').get();
+    for (QueryDocumentSnapshot doc in result.docs) {
+      matchList.add(
+        Football(
+          matchName: doc.id,
+          team1Name: doc.get('team1Name'),
+          team2Name: doc.get('team2Name'),
+          team1Score: doc.get('team1'),
+          team2Score: doc.get('team2'),
+        ),
+      );
+    }
+    setState(() {
+
+    });
+  }
+
+  @override
+  void initState() {
+    _getFootballMatches();
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,24 +49,47 @@ class _MatchListScreenState extends State<MatchListScreen> {
         title: Text('Football'),
         backgroundColor: Colors.blue,
       ),
-      body: ListView.builder(
-          itemCount: 3,
-          itemBuilder: (context, index){
-        return Card(
-          child: Row(
-            children: [
-              Column(
-                children: [
-                  Text('1'),
-                  Text('1'),
-                  Text('1'),
-                  Text('1'),
-                ],
-              )
-            ],
-          ),
-        );
-      }),
+      body: StreamBuilder(
+        stream: firebaseFirestore.collection('football').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+
+          if (snapshot.hasData == false) {
+            return const Center(
+              child: Text('Empty list'),
+            );
+          }
+          matchList.clear();
+          for (QueryDocumentSnapshot doc in snapshot.data?.docs ?? []) {
+            matchList.add(
+              Football(
+                matchName: doc.id,
+                team1Name: doc.get('team1Name'),
+                team2Name: doc.get('team2Name'),
+                team1Score: doc.get('team1'),
+                team2Score: doc.get('team2'),
+              ),
+            );
+          }
+          return ListView.builder(
+              itemCount: matchList.length,
+              itemBuilder: (context, index){
+            return FootballScoreCard(football: matchList[index],);
+          });
+        }
+      ),
     );
   }
 }
+
+
